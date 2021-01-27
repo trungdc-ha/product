9d8a3868d201cf53a1bd2fee41362bc9904161d4
trungdc-ha <dangchitrungha@gmail.com>
2021-01-27 11:00:28 +0700

    TrungDC - update config

--- src/main/java/vn/codegym/product/config/AppConfiguration.java
+++ src/main/java/vn/codegym/product/config/AppConfiguration.java
@@ -3,11 +3,12 @@ package vn.codegym.product.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /*thay the cho file dispatcher-servlet.xml*/
 @Configuration
-public class AppConfiguration extends WebMvcConfigurerAdapter {
+public class AppConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("WEB-INF/js/");
