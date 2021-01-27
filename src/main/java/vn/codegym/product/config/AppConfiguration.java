486336aff8c644aa8666b61812af04f56f462ed4
trungdc-ha <dangchitrungha@gmail.com>
2021-01-27 11:06:05 +0700

    TrungDC - update config

--- src/main/java/vn/codegym/product/config/AppConfiguration.java
+++ src/main/java/vn/codegym/product/config/AppConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("WEB-INF/js/");
-        registry.addResourceHandler("/img/**").addResourceLocations("WEB-INF/img/");
+        registry.addResourceHandler("/css/**").addResourceLocations("WEB-INF/classes/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("WEB-INF/classes/static/js/");
+        registry.addResourceHandler("/img/**").addResourceLocations("WEB-INF/classes/static/img/");
     }
 }
